ce552d09bef693a1669ebd13c1499a885671f0ae
dimitri <dev@perriet.fr>
2025-03-17 09:54:15 +0100

    Modification de l'URL de base de l'API dans apiConfig.js pour passer de l'API hébergée sur Vercel à l'API locale sur le port 3001.

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configuration de base d'Axios
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://katapult-api.vercel.app/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001/api',
   headers: {
     'Content-Type': 'application/json'
   }
